a1a645961f6d36c6f6da68b19519666e261488cb
kinglove58 <obafemielijahsunday@gmail.com>
2025-07-18 10:13:08 +0100

    Refactor cookie settings in generateToken function for enhanced security

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,9 +7,9 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true,
-    sameSite: "None",
-    secure: true,
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: process.env.NODE_ENV === "development" ? "strict" : "none", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development", // only send cookie over HTTPS in production
   });
 
   return token;
